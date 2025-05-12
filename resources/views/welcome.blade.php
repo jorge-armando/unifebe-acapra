f3976784a936cd45d76314bf2ae8f38550a4822b
Jorge Armando <jorge.vermohlen@gmail.com>
2025-05-12 20:29:04 -0300

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        <link rel="stylesheet" href="{{ asset('css/10-reset.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/20-tokens.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/30-fonts.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/40-base.css') }}">
+    </head>
+    <body>
+        <h1>Teste</h1>
+        <p>wedawe</p>
+    </body>
+</html>
