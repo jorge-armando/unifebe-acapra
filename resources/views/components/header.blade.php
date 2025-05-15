b3259eaa4f6cded46fbe693aa63c5eae70e6f516
jao <joaogabrielcosta_biel@hotmail.com>
2025-05-15 20:35:32 -0300

    nevagação entre telas do adm completa

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header id="website-header">
-    <a style="margin-top: -50px; display: inline-block;" href="/login">
+    <a style="margin-top: -50px; display: inline-block;" href="/admin/login">
         <img id="imgSouAcapra" src="{{ asset('images/iconUs.png') }}" alt="Imagem termos e condições">
     </a>
     <div id="logo-container">
--- resources/views/components/headerAdm.blade.php
+++ resources/views/components/headerAdm.blade.php
@@ -3,12 +3,12 @@
         <img id="logo" src="{{ asset('images/iconLogo.png') }}" alt="Logo ACAPRA">
         
         <nav class="nav-links">
-            <a href="/admin/home">Home</a>
+            <a href="/admin">Home</a>
             <a href="/admin/forms">Formulários</a>
-            <a href="/cadastro">Cadastrar Admin</a>
-            <a href="/admin/addpet">Adicionar Pet</a>
+            <a href="/admin/cadastro">Cadastrar Admin</a>
+            <a href="/admin/pets/add">Adicionar Pet</a>
             
-            <a href="/user/home" class="admin-user">
+            <a href="/" class="admin-user">
                 Olá, Jorge
                 <img src="{{ asset('images/sair.png') }}" alt="Sair" style="height: 24px;">
             </a>
