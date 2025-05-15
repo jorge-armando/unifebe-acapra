273530d58d00f8397157721c9ebb28ac85742113
jao <joaogabrielcosta_biel@hotmail.com>
2025-05-15 20:26:26 -0300

    Caminhos para as telas e algumas correções

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,7 @@
 <header id="website-header">
-    <img id="imgSouAcapra" src="{{ asset('images/iconUs.png') }}" alt="Imagem termos e condições">
+    <a style="margin-top: -50px; display: inline-block;" href="/login">
+        <img id="imgSouAcapra" src="{{ asset('images/iconUs.png') }}" alt="Imagem termos e condições">
+    </a>
     <div id="logo-container">
         <img id="logo" src="{{ asset('images/iconLogo.png') }}" alt="Imagem termos e condições">
     </div>
--- resources/views/components/headerAdm.blade.php
+++ resources/views/components/headerAdm.blade.php
@@ -3,11 +3,12 @@
         <img id="logo" src="{{ asset('images/iconLogo.png') }}" alt="Logo ACAPRA">
         
         <nav class="nav-links">
-            <a href="/formularios">Formulários</a>
-            <a href="/admin/cadastrar">Cadastrar Admin</a>
+            <a href="/admin/home">Home</a>
+            <a href="/admin/forms">Formulários</a>
+            <a href="/cadastro">Cadastrar Admin</a>
             <a href="/admin/addpet">Adicionar Pet</a>
             
-            <a href="/admin/Sair" class="admin-user">
+            <a href="/user/home" class="admin-user">
                 Olá, Jorge
                 <img src="{{ asset('images/sair.png') }}" alt="Sair" style="height: 24px;">
             </a>
