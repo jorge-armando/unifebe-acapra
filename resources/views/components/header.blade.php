9869cbdbf331421f1265d734bdc2971eca97a0ea
Jorge Armando <jorge.vermohlen@gmail.com>
2025-10-27 20:39:09 -0300

    Latest adjustments

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
         <img id="imgSouAcapra" src="{{ asset('images/iconUs.png') }}" alt="Imagem termos e condições">
     </a>
     <div id="logo-container">
-        <a href="/pets">
+        <a href="/">
             <img id="logo" src="{{ asset('images/iconLogo.png') }}" alt="Logo">
         </a>
 
